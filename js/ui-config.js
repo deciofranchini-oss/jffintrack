b334cb436903e46f7aee9f2e888bf102ecabc970
Décio <deciofranchini@gmail.com>
2026-03-10 09:26:02 -0300

    Add files via upload

--- js/ui-config.js
+++ js/ui-config.js
@@ -3,5 +3,5 @@
   Change values here without touching core code.
 */
 window.UI_CONFIG = {
-  SIDEBAR_LOGO_URL: "https://your-domain.com/logo.png" // <-- set your sidebar logo URL here
+  SIDEBAR_LOGO_URL: "https://your-domain.com/logo.jpg" // <-- set your sidebar logo URL here
 };
